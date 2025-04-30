ca04b95eb6e42cb0863fbc7346d347ad429178d6
NamikaseEXE <carlos0012010vegano@gmail.com>
2025-04-30 16:59:15 -0400

    app basica terminada con iconos

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Note } from '@/generated/prisma/client'
 import { useNotes } from '@/context/NoteContext'
+import { HiTrash, HiPencil } from 'react-icons/hi'
 
 export default function NoteCard({ note }: { note: Note }) {
     const { deleteNote, setSelectedNote } = useNotes();
@@ -9,15 +10,19 @@ export default function NoteCard({ note }: { note: Note }) {
             <div>
                 <h2 className='text-2xl font-bold'>{note.title}</h2>
                 <p>{note.content}</p>
+                <p> Creada el: {new Date(note.createdAt).toLocaleDateString()}</p>
             </div>
             <div className='flex gap-x-2'>
                 <button onClick={async () => {
                     if (confirm('Are you sure you want to delete this note?')) {
                         await deleteNote(Number(note.id))
                     }
-                }}>Delete</button>
+                }}>
+                    <HiTrash className='text-2xl text-red-600'/>
+                </button>
                 <button
-                    onClick={() => setSelectedNote(note)}>Edit
+                    onClick={() => setSelectedNote(note)}>
+                        <HiPencil className='text-2xl'/>
                 </button>
             </div>
         </div>
--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -51,7 +51,8 @@ export default function NoteForm() {
 
       <div className='flex justify-end gap-x-2'>
         <button type="submit" className='px-5 py-2 text-white bg-blue-600 rounded-md
-        hover:bg-blue-700'>
+        hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed'
+        disabled={!title || !content}>
           {
             selectedNote ? 'Update' : 'Create'
           }
